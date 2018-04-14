a31a4d41bd6f7b4013ed7c6fb32e918283e05c3e
Mosses <musicmojica@hotmail.com>
2018-04-14 15:35:33 -0600

    Add project files.

---
+++ Proyecto/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto.Controllers
+{
+    public class DefaultController : Controller
+    {
+
+        private TablaDato tabladato = new TablaDato();
+
+        public int Index()
+        {
+
+
+            return tabladato.Conteo();
+        }
+    }
+}
