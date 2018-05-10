8d965e41043582ff24e03055f2c251c94fd7a103
adm_mmojica <adm_mmojica@CE-IT01-SP>
2018-05-10 15:57:23 -0600

    testing testimonios

--- Proyecto/Areas/Admin/Controllers/TestimoniosController.cs
+++ Proyecto/Areas/Admin/Controllers/TestimoniosController.cs
@@ -4,16 +4,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Model;
+using Helper;
 
 namespace Proyecto.Areas.Admin.Controllers
 {
     [Autenticado]
     public class TestimoniosController : Controller
     {
+
+        private Testimonio testimonio = new Testimonio();
         // GET: Admin/Testimonios
         public ActionResult Index()
         {
             return View();
         }
+
+        public JsonResult Listar(AnexGRID grid)
+        {
+            return Json(testimonio.Listar(grid,SessionHelper.GetUser() ));
+
+        }
+
+        public ActionResult Crud (int id)
+        {
+            return View(testimonio.Obtener(id)); 
+
+        }
+
+        public JsonResult Eliminar(int id) {
+            return Json(testimonio.Eliminar(id));
+
+        }
+
+        public JsonResult Guardar(Testimonio model) {
+
+            var rm = new ResponseModel();
+
+            if (ModelState.IsValid)
+            {
+                rm = model.Guardar();
+
+                if (rm.response)
+                {
+                    rm.href = Url.Content("~/admin/testimonios/");
+                }
+
+            }
+
+            return Json(rm);
+
+        }
     }
+
+
+
 }
